caabd150ac86037d36e8c5f3077c0d8170c20062
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2024-12-26 16:49:45 +0100

    wip: CupertinoTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +9,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      theme: CupertinoThemeData(
+        brightness: Brightness.dark,
+        textTheme: CupertinoTextThemeData(
+          primaryColor: Color(0xFFF0DDA2)
+        )
       ),
-    );
+    )
+    ;
   }
 }
