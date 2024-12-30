6a3fbd0cec626f4f877019d822f8eae0a57a4533
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2024-12-30 14:58:00 +0100

    wip: created cupertinoCustomThemeLight

---
+++ lib/theme/cupertino_light_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+const CupertinoThemeData cupertinoCustomThemeLight = CupertinoThemeData(
+    brightness: Brightness.light,
+    primaryColor: Color(0xFFF0DDA2),
+    primaryContrastingColor: Color(0xFF713024),
+    // TODO add textTheme
+    barBackgroundColor: Color(0xFF342428),
+    scaffoldBackgroundColor: Color(0xFF2A0505));
