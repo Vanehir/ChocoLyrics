dacd51073ca92a4c9779cf925105262d11f444e5
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-12 19:49:01 +0100

    feat: added home, favorite, search placeholders

---
+++ lib/screens/favorites/favorite_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: Text('favorites coming soon'),
+      ),
+    );
+  }
+}
