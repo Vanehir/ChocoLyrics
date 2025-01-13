f14fc4e19903e1d18989e0bd0fa1817fa41ace83
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-13 11:42:11 +0100

    chore: cherry pick models from branch api

---
+++ lib/data/models/album.dart
@@ -0,0 +1,29 @@
+import 'package:choco_lyrics/data/models/artist.dart';
+
+class Album {
+  final String id;
+  final String name;
+  final List<Artist> artists;
+  final String coverUrl;
+  final int totalTracks;
+
+  Album({
+    required this.id,
+    required this.name,
+    required this.artists,
+    required this.coverUrl,
+    required this.totalTracks,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      id: json['id'] ?? '',
+      name: json['name'] ?? '',
+      artists: ((json['artists'] as List?) ?? [])
+          .map((a) => Artist.fromJson(a))
+          .toList(),
+      coverUrl: json['images']?[0]?['url'] ?? '',
+      totalTracks: json['total_tracks'] ?? 0,
+    );
+  }
+}
