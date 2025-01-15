d454a4476aa9432583f04d34ee9dedb0bdb82f4c
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-15 15:08:47 +0100

    feat: added get access token

---
+++ lib/data/repositories/spotify/spotify_access_token.dart
@@ -0,0 +1,44 @@
+import 'package:choco_lyrics/core/secure_storage.dart';
+import 'package:choco_lyrics/utilities/base64_encode.dart';
+import 'package:dio/dio.dart';
+import 'package:choco_lyrics/data/repositories/spotify/spotify_constants.dart';
+
+class GetSpotifyAccessToken {
+  final Dio dio = Dio();
+  final SecureStorage secureStorage = SecureStorage();
+
+  Future<void> getAccessToken() async {
+    print('SSUUSS');
+    final response = await dio.post(
+      tokenUrl,
+      data: {'grant_type': 'client_credentials'},
+
+      // this is for the Authorization Code with PKCE, we do not need it right now, and maybe will never need it
+      /*
+      data: FormData.fromMap({
+        'client_id': clientId,
+        'grant_type': 'authorization_code',
+        'code_verifier': codeVerifier,
+      }),
+      */
+
+      options: Options(
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization':
+              'Basic ${base64EncodeString('$clientId:$clientSecret')}',
+        },
+      ),
+    );
+    print("SSAASS");
+    if (response.statusCode == 200 && response.data != null) {
+      print("SS00SS");
+      secureStorage.setItem(
+          key: accessTokenKey, item: response.data[accessTokenKey]);
+      print('qua???');
+    } else {
+      throw Exception(
+          'Failed to authenticate with Spotify: ${response.statusMessage}');
+    }
+  }
+}
