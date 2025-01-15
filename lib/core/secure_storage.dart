b8aba679eb66ca33f973094f36805c6d40895003
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-15 15:10:19 +0100

    feat: created secure storage

--- lib/core/secure_storage.dart
+++ lib/core/secure_storage.dart
@@ -1,31 +1,28 @@
-import 'dart:developer';  
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';  
+import 'dart:developer';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class SecureStorage {  
-  final _storage = const FlutterSecureStorage(); 
+class SecureStorage {
+  final _storage = const FlutterSecureStorage();
 
-  Future<void> setItem(String key, String item) async {  
-    try {  
-      await _storage.write(key: key, value: item);    
-    } catch (e) {  
-      throw Exception("Error setting item: $e"); 
-    }  
-  }  
+  Future<void> setItem({required String key, required String item}) async {
+      await _storage.write(key: key, value: item);
 
-  Future<String?> getItem(String key) async {  
-    try {  
-      return await _storage.read(key: key);  
-    } catch (e) {  
-      throw Exception("Error getting item: $e");  
-    }  
-  }  
+  }
 
-  Future<void> clearStorage() async {  
-    try {  
-      await _storage.deleteAll();  
-      log("Storage cleared successfully!");  
-    } catch (e) {  
-      log("Error clearing storage: $e");  
-    }  
-  }  
+  Future<String?> getItem({required String key}) async {
+    try {
+      return await _storage.read(key: key);
+    } catch (e) {
+      throw Exception("Error getting item: $e");
+    }
+  }
+
+  Future<void> clearStorage() async {
+    try {
+      await _storage.deleteAll();
+      log("Storage cleared successfully!");
+    } catch (e) {
+      log("Error clearing storage: $e");
+    }
+  }
 }
