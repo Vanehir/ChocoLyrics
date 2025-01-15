05d2dc7205590dde2c65577f96d9c893d73a7396
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-15 20:32:55 +0100

    chore: changed playlist ids

--- lib/data/repositories/spotify/spotify_constants.dart
+++ lib/data/repositories/spotify/spotify_constants.dart
@@ -1,9 +1,11 @@
 const String tokenUrl = 'https://accounts.spotify.com/api/token';
 const String searchUrl = 'https://api.spotify.com/v1/search';
 const String playListUrl = 'https://api.spotify.com/v1/playlists/';
-const String top50GlobalId = '37i9dQZEVXbMDoHDwVN2tF';
-const String todayTopHits = '37i9dQZF1DXcBWIGoYBM5M';
+const String top50GlobalId = '37i9dQZEVXbMDoHDwVN2tF'; // to change
+const String todayTopHits = '3Xta6685JStp5fttgaWP7m'; // to change
 const String clientId = '668b2dc4f8b54374a4e7b6f7438ba18e';
 const String clientSecret = '904e16e024624847b2651c38bf7a9041';
 const String accessTokenKey = 'access_token';
 const String refreshTokenKey = 'refresh_token';
+
+
