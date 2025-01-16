b3ba9ec7603de1bad5ee9ea71dd6f49978927146
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-16 12:33:02 +0100

    feat: added lrclib repository

---
+++ lib/data/repositories/lrclib/lrclib_repository.dart
@@ -0,0 +1,33 @@
+import 'package:choco_lyrics/data/repositories/lrclib/lrclib_constants.dart';
+import 'package:dio/dio.dart';
+
+class LrcLibRepository {
+  final Dio dio = Dio();
+
+  Future<List<String>?> getLyrics(
+      {required String artistName, required String songName}) async {
+    try {
+      final response = await dio.get(
+        '$searchUrl $artistName $songName',
+        options: Options(
+          headers: {
+            'Lrclib-Client':
+                'ChocoLyrics (https://github.com/Vanehir/ChocoLyrics)'
+          },
+        ),
+      );
+      print('url: $searchUrl $artistName $songName');
+      print('status code: ${response.statusCode}');
+      if (response.statusCode == 200) {
+        final lyrics = (response.data[0]['plainLyrics'] as String).split('\n');
+        return lyrics;
+      } else if (response.statusCode == 404) {
+        return null;
+      } else {
+        throw Exception('Failed to get lyrics');
+      }
+    } catch (e) {
+      throw Exception('Error getting lyrics: $e');
+    }
+  }
+}
