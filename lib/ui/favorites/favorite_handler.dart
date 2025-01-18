2edf1fc1993d4cdaf70aee38ae97deb908cf8918
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-18 14:53:19 +0100

    feat: created favorite_handler

---
+++ lib/ui/favorites/favorite_handler.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:choco_lyrics/core/storage/secure_storage.dart';
+
+const String favoritesKey = 'favorites';
+
+class FavoriteHandler {
+  final SecureStorage secureStorage = SecureStorage();
+
+  Future<List<String>> getFavorites() async {
+    final String? stringFavorites =
+        await secureStorage.getItem(key: favoritesKey);
+    final List<dynamic> favorites = jsonDecode(stringFavorites ?? '[]');
+    return favorites.cast<String>();
+  }
+
+  Future<void> addFavorite(String id) async {
+    final String? stringFavorites =
+        await secureStorage.getItem(key: favoritesKey);
+    final List<dynamic> favorites = jsonDecode(stringFavorites ?? '[]');
+    favorites.add(id);
+    await secureStorage.setItem(key: favoritesKey, item: jsonEncode(favorites));
+  }
+
+  Future<void> removeFavorite(String id) async {
+    final String? stringFavorites =
+        await secureStorage.getItem(key: favoritesKey);
+    final List<dynamic> favorites = jsonDecode(stringFavorites ?? '[]');
+    favorites.remove(id);
+    await secureStorage.setItem(key: favoritesKey, item: jsonEncode(favorites));
+  }
+}
